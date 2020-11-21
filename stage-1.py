2b00690c55719f4148d72cc828abdd7d458d483a
Nomthi-B <Nomtee.b@gmail.com>
2020-11-21 16:47:56 +0200

    done

--- stage-1.py
+++ stage-1.py
@@ -1,2 +1,7 @@
-# begin by placing the cup in the machine
-# add your desired sugar amount
+print ("Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!")
